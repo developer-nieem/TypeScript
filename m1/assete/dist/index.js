f83f6a6206aea02d19a3aaf45bb689be22637cee
developer-nieem <naimhossenwork@gmail.com>
2023-11-08 17:55:00 +0600

    typescript practics

---
+++ m1/assete/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let name1 = "name";
+let num1 = 30;
+let isNumber = true;
+// const arr : [string , number] = ["a", 'b']
+const person = {
+    company: "SUbtle It",
+    firstName: "nieem",
+    lastName: 'Hossen',
+    salary: 20000,
+};
+console.log(person.salary);
---
+++ m1/assete/ts/index.ts
@@ -0,0 +1,29 @@
+
+
+
+let name1 : string =  "name"
+
+let num1 : number =  30;
+
+let isNumber : boolean = true
+
+
+
+// const arr : [string , number] = ["a", 'b']
+
+
+
+
+const person : {
+company : string,
+firstName : string,
+lastName : string,
+salary : number,
+}=  {
+    company : "SUbtle It",
+    firstName : "nieem",
+    lastName : 'Hossen',
+    salary : 20000,
+}
+
+console.log(person.salary);
